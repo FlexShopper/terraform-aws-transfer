fe0125bf881bf94aa8a67d1797cbf743793a8506
Steven Almonte <stevenalmonte@gmail.com>
2021-04-01 16:26:28 -0400

    WIP

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 
 output "transfer_sftp_server_arn" {
-  value = aws_transfer_server.sftp[count.index].arn
+  value = aws_transfer_server.sftp.arn
 }
 
 output "transfer_sftp_server_id" {
-  value = aws_transfer_server.sftp[count.index].arn
+  value = aws_transfer_server.sftp.arn
 }
 
 output "transfer_sftp_server_endpoint" {
-  value = aws_transfer_server.sftp[count.index].endpoint
+  value = aws_transfer_server.sftp.endpoint
 }
 
 output "transfer_sftp_host_key_fingerprint" {
-  value = aws_transfer_server.sftp[count.index].host_key_fingerprint
+  value = aws_transfer_server.sftp.host_key_fingerprint
 }
