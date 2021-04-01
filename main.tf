314d8e797d10d88e9d6415c3219d06b3a62c83da
Steven Almonte <stevenalmonte@gmail.com>
2021-04-01 16:19:38 -0400

    WIP

--- main.tf
+++ main.tf
@@ -55,6 +55,7 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "this_logging_role" {
+  count = var.create_sftp_server ? 1 : 0
   role       = aws_iam_role.this_logging_role[count.index].name
   policy_arn = aws_iam_policy.this_logging_role_policy[count.index].arn
 }
