4c847ad098456b21d3ad65aea81c8609836049e4
Steven Almonte <stevenalmonte@gmail.com>
2021-04-01 16:04:44 -0400

    WIP

--- main.tf
+++ main.tf
@@ -55,8 +55,8 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "this_logging_role" {
-  role       = aws_iam_role.this_logging_role[count.index].name
-  policy_arn = aws_iam_policy.this_logging_role_policy[count.index].arn
+  role       = aws_iam_role.this_logging_role.name
+  policy_arn = aws_iam_policy.this_logging_role_policy.arn
 }
 
 ## Transfer - SFTP
