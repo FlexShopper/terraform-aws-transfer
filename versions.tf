05b9458ef219b4a7f762d2dbd79c788d42c3e312
Steven Almonte <stevenalmonte@gmail.com>
2021-04-02 17:52:22 -0400

    WIP

--- versions.tf
+++ versions.tf
@@ -5,14 +5,7 @@ terraform {
 
   required_providers {
     
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0.0"
-    }
+    aws = "~> 3.0.0"
+    null = "~> 3.0.0"
   }
 }
