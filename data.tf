d4795fbf17c35cbac85345089d8fdfe18cf044fa
Steven Almonte <steven.almonte@flexshopper.com>
2021-05-11 00:20:43 -0400

    Fix: Adding count for route53 zone id lookup

--- data.tf
+++ data.tf
@@ -2,6 +2,7 @@
 data "aws_caller_identity" "this" {}
 data "aws_region" "this" {}
 data "aws_route53_zone" "this" {
+  count = var.custom_hostname_route53 != null ? 1 : 0
   name = var.route53_zone_name
 }
 
