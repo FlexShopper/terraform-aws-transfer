9e507628f842c0d8128f464894afa22b85ce0126
kyvidmar <93599908+kyvidmar@users.noreply.github.com>
2023-02-22 09:21:52 -0500

    Delete data.tf

--- data.tf
+++
@@ -1,7 +0,0 @@
-
-data "aws_caller_identity" "this" {}
-data "aws_region" "this" {}
-data "aws_route53_zone" "this" {
-  count = var.route53_zone_name != null ? 1 : 0
-  name = var.route53_zone_name
-}
